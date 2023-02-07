ee88fd8ef4159a817f206054d857ddd04de0c632
bob <ricardo.silvestre.1995@gmail.com>
2023-02-07 15:49:38 +0000

    first commit

---
+++ libs/tests/CMakeFiles/empty.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/eeeri/glfw/glfw/deps/tinycthread.c" "tests/CMakeFiles/empty.dir/__/deps/tinycthread.c.obj" "gcc" "tests/CMakeFiles/empty.dir/__/deps/tinycthread.c.obj.d"
+  "C:/Users/eeeri/glfw/glfw/tests/empty.c" "tests/CMakeFiles/empty.dir/empty.c.obj" "gcc" "tests/CMakeFiles/empty.dir/empty.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "F:/myProjects/cpp_vulkan/libs/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+
+#include "../pre/pch.h"
+#include <GLFW/glfw3.h>
+
+void glfwCode(){
+    if (!glfwInit())
+    {
+        // Initialization failed
+    }
+}
+
+
+int main( int argc, char**argv){
+    std::cout << "program starting\n";
+
+    return 0;
+}
